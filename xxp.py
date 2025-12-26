db91e0ea1e27e5a779603a0b4c8fb0263676d016
Bhavini <BHAVINI.22584@stu.upes.ac.in>
2025-12-26 17:03:21 +0530

    daily commit

---
+++ xxp.py
@@ -0,0 +1,10 @@
+import random
+num = random.randint(0, 5)
+if num == 5 :
+ print('It is illegal to sing off-key in North Carolina')
+elif num == 4 :
+  print('it is impossible to sneeze while sleeping')
+elif num == 3 :
+  print('A taste bud\'s life span is about 10 days')
+elif num == 2 :
+  print('Shrimp can only swim backwards')
---
+++ xxxp.py
@@ -0,0 +1,27 @@
+earth_weight = float(input("Enter your weight on Earth (in kg): "))
+
+
+planet = int(input("Enter the planet number (1-7): "))
+
+if planet == 1:
+    gravity = 0.38      
+elif planet == 2:
+    gravity = 0.91      
+elif planet == 3:
+    gravity = 0.38      
+elif planet == 4:
+    gravity = 2.53      
+elif planet == 5:
+    gravity = 1.07      
+elif planet == 6:
+    gravity = 0.89      
+elif planet == 7:
+    gravity = 1.14      
+else:
+    gravity = None
+
+if gravity is None:
+    print("Invalid planet number")
+else:
+    destination_weight = earth_weight * gravity
+    print("Your weight on the selected planet is:", destination_weight)
